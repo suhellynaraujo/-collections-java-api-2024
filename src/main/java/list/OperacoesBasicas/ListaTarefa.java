c92e25f5a409e8e42745dbb5434f3934022770b2
suhellynaraujo <github.interface@gmail.com>
2024-06-25 21:19:22 -0300

    remove

--- src/main/java/list/OperacoesBasicas/ListaTarefa.java
+++ src/main/java/list/OperacoesBasicas/ListaTarefa.java
@@ -16,5 +16,17 @@ public class ListaTarefa {
     public void adicionarTarefa(String descricao){
         tarefaList.add(new Tarefa(descricao));
     }
+
+    //remover
+    public void removerTarefa(String descricao){
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for (Tarefa t: tarefaList) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)){
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+
    
 }
